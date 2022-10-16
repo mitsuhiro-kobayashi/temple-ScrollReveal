b84de87164c7c67d902960925de73c018edbc4c9
mitsuhiro.kobayashi <zukunftxiaolin@gmail.com>
2022-10-16 15:35:20 +0900

    scrolltriggerで文字も消せた

--- js/script.js
+++ js/script.js
@@ -1,16 +1,31 @@
+//sectionのふわっと出てくるアニメーション
 const sections = document.querySelectorAll('section')
 
 sections.forEach(el => {
     gsap.from(el, .5, {
         opacity: 0,
         y: 20,
-        scale: 0.8,
-        ease: 'back',
+        scale: 0.6,//伸縮
+        // ease: 'back',
         scrollTrigger: {
             trigger: el,
-            start: '10% 80%',
+            start: 'top 80%',
             end: '+=300px',
-            markers: true,
+            // markers: true,
+            scrub: true,//スクロール量に応じて動く
+            // once: true,//一回こっきりかどうか
         }
     })
+})
+
+gsap.to('.firstview', 1, {
+    opacity: 0,
+    scrollTrigger: {
+        trigger: '.firstview',
+        start: '30% 20%',
+        end: '70% 20%',
+        opacity: 1,
+        markers: true,
+        scrub: true,
+    }
 })
